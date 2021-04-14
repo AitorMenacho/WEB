7384b9934917de3efe3f1315c5f63f6da00cb394
AitorMenacho <aitormenachovega@cdpjosecabrera.es>
2021-04-14 19:39:04 +0200

    Inicio casi finalizado

--- Página WEB/index.js
+++ Página WEB/index.js
@@ -1,10 +1,25 @@
-$(function() {
-    $('.habilidad').easyPieChart({
-      scaleColor: false,
-      lineWidth: 20,
-      lineCap: 'butt',
-      barColor: 'black',
-      size: 150,
-      animate: 1500
-    });
+let options = {
+    startAngle: -1.55,
+    size: 150,
+    value: 0.85,
+    fill: {gradient: ['#fff']}
+  }
+  $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+  function(event, progress, stepValue){
+    $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+  $(".js .bar").circleProgress({
+    value: 0.40
+  });
+  $(".php .bar").circleProgress({
+    value: 0.50
+  });
+  $(".sql .bar").circleProgress({
+    value: 0.40
+  });
+  $(".wordpress .bar").circleProgress({
+    value: 0.85
+  });
+  $(".wix .bar").circleProgress({
+    value: 0.90
   });
