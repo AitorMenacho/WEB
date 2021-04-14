0dcc7dbb030c8045b62376744e274a51e6c47226
AitorMenacho <aitormenachovega@cdpjosecabrera.es>
2021-04-14 10:40:07 +0200

    Página WEB
    Página WEB completa

---
+++ Nuevo Proyecto/index.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.habilidad').easyPieChart({
+      scaleColor: false,
+      lineWidth: 20,
+      lineCap: 'butt',
+      barColor: 'black',
+      size: 150,
+      animate: 1500
+    });
+  });
